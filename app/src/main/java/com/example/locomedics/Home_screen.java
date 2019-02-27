0c85b587d529eed786f6de40e9c4f63fcf5745c6
LokeshRs <lokesh.rs12@gmail.com>
2019-02-27 20:41:39 +0530

    DataBase Connected

--- app/src/main/java/com/example/locomedics/Home_screen.java
+++ app/src/main/java/com/example/locomedics/Home_screen.java
@@ -3,8 +3,10 @@ package com.example.locomedics;
 import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,11 +16,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Home_screen extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     ClipData.Item home;
     Menu menu;
+    FirebaseDatabase firebaseDatabase;
+    Button search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +57,29 @@ public class Home_screen extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        search =findViewById(R.id.search_button);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseDatabase =FirebaseDatabase.getInstance();
+                DatabaseReference db_ref= firebaseDatabase.getReference();
+                DatabaseReference db_child=db_ref.child("0");
+                DatabaseReference db_sub =db_child.child("Med_discription");
+                db_sub.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String value = dataSnapshot.getValue(String.class);
+                        Log.d("DATABASE",value);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Log.d("DATABASE","error");
+                    }
+                });
+            }
+        });
+
     }
 
     @Override
@@ -103,6 +137,7 @@ public class Home_screen extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+
     }
 
 }
